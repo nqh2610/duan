71168bf65e0595648b1728f8c6e35c1d280b7be4
hungnq77 <hungnq77@fpt.edu.vn>
2024-10-04 22:27:19 +0700

    cap nhat model tien

--- js/nhan_dang_tien.js
+++ js/nhan_dang_tien.js
@@ -1,5 +1,5 @@
 // const URL = "https://teachablemachine.withgoogle.com/models/_VoeOrhBw/";
-const URL ="https://teachablemachine.withgoogle.com/models/8Y8THkMDG/"
+const URL ="https://teachablemachine.withgoogle.com/models/gdwBzqWuO/"
 let model, webcam, labelContainer, maxPredictions, highestPrediction, highestProbability;
 
 // Automatically call init when the page loads
