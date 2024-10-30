c071ef4f44ba8b7862fb53d39646e9bb01be3a47
unknown <you@example.com>
2024-10-30 09:06:10 +0700

    cap nhat model tim do vat

--- js/tim_do_vat.js
+++ js/tim_do_vat.js
@@ -1,4 +1,4 @@
-const URL = "https://teachablemachine.withgoogle.com/models/Ud020KBcy/";
+const URL = "https://teachablemachine.withgoogle.com/models/0fC1vRKVC/";
 
 let model, webcam, labelContainer, maxPredictions, highestPrediction, highestProbability;
 let lastSpokenTime = 0; // Theo dõi thời gian của cảnh báo được nói lần cuối
